680e96ff60c5e2bf89f21f1dd3b55fb6f32d1086
joshua stein <jcs@jcs.org>
2015-09-30 12:52:59 -0500

    clean up c code, set watchface to true in appinfo

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# pebble-hotdogstand
+###Hot Dog Stand
+
+An ode to the Windows 3.1 Hot Dog Stand theme.
+
+Looks best in color on a Pebble Time but should work on an older Pebble.
+
+Available in the [Pebble App Store](https://apps.getpebble.com/en_US/application/560bfdaf4bf0ac562000007d).
+
+![screenshot.png](screenshot.png)
+
+![win31.png](win31.png)
--- src/main.c
+++ src/main.c
@@ -1,5 +1,32 @@
+/*
+ * Copyright (c) 2015 joshua stein <jcs@jcs.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <pebble.h>
-  
+
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_date_layer;
@@ -7,38 +34,36 @@ static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 
 static void update_time() {
-  // Get a tm structure
-  time_t temp = time(NULL); 
+  static char buffer[] = "  :     ";
+  static char dbuffer[] = "  /  /  ";
+
+  time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
 
-  if(clock_is_24h_style() == true) {
-    static char buffer[] = "00:00";
+  if (clock_is_24h_style() == true) {
     strftime(buffer, sizeof(buffer), "%H:%M", tick_time);
     text_layer_set_text(s_time_layer, buffer);
-  } else {
-    static char buffer[] = "00:00 AM";
+  }
+  else {
     strftime(buffer, sizeof(buffer), "%I:%M %p", tick_time);
     text_layer_set_text(s_time_layer, buffer);
   }
-  
-  static char dbuffer[] = "00/00/00";
+
   strftime(dbuffer, sizeof(dbuffer), "%m/%e/%y", tick_time);
   text_layer_set_text(s_date_layer, dbuffer);
 }
 
 static void main_window_load(Window *window) {
-  // Create GBitmap, then set to created BitmapLayer
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
   s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
-  
-  // Create time TextLayer
+
   s_time_layer = text_layer_create(GRect(0, 65, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   if (clock_is_24h_style() == true) {
-    layer_set_frame(text_layer_get_layer(s_time_layer), GRect(0, 70, 144, 50)); 
+    layer_set_frame(text_layer_get_layer(s_time_layer), GRect(0, 70, 144, 50));
     text_layer_set_text(s_time_layer, "  :  ");
     text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_34_MEDIUM_NUMBERS));
   }
@@ -46,62 +71,47 @@ static void main_window_load(Window *window) {
     text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
     text_layer_set_text(s_time_layer, "  :    ");
   }
-  // Improve the layout to be more like a watchface
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-  // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
-  
+
   s_date_layer = text_layer_create(GRect(0, 95, 144, 50));
   if (clock_is_24h_style() == true)
     layer_set_frame(text_layer_get_layer(s_date_layer), GRect(0, 100, 144, 50));
-  
+
   text_layer_set_background_color(s_date_layer, GColorClear);
   text_layer_set_text_color(s_date_layer, GColorBlack);
   text_layer_set_text(s_date_layer, "  /  /  ");
-  // Improve the layout to be more like a watchface
   text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
   text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
-  // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
 
-  // Make sure the time is displayed from the start
   update_time();
 }
 
 static void main_window_unload(Window *window) {
-  // Destroy TextLayer
   text_layer_destroy(s_time_layer);
-  
-  // Destroy GBitmap
   gbitmap_destroy(s_background_bitmap);
-  
-  // Destroy BitmapLayer
   bitmap_layer_destroy(s_background_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
 }
-  
+
 static void init() {
-  // Create main Window element and assign to pointer
   s_main_window = window_create();
 
-  // Set handlers to manage the elements inside the Window
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
     .unload = main_window_unload
   });
 
-  // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
-  
-  // Register with TickTimerService
+
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit() {
-  // Destroy Window
   window_destroy(s_main_window);
 }
 
